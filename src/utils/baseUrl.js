80e73d447fdcf9df30e7e6336e75cb3dd696ae24
deepakbhadoriya <deepakkbhadoriya@gmail.com>
2021-03-31 14:33:42 +0530

    base url setup

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 let baseUrl = `http://localhost:5000`;
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
   baseUrl = 'https://devconnect7.herokuapp.com';
 } else {
   baseUrl = 'http://localhost:5000';
